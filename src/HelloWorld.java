e58166173f19a07bbc3a4cfbd99cee8337a822e0
JuanEladio04 <jparmar1@iesmarquesdecomares.org>
2022-09-20 17:18:21 +0200

    Prueba Visual Code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world ");
+		System.out.println("Hello world");
+		System.out.println("Prueba");
+
 	}
 
 }
