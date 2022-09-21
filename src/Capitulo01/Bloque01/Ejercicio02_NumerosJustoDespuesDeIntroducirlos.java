3dd5fcc1c960152ff36c4a558839ad714b10f367
JuanEladio04 <jparmar1@iesmarquesdecomares.org>
2022-09-21 18:06:21 +0200

    feat (Chapter 1): Added exercises 3, 4 and 5.

--- src/Capitulo01/Bloque01/Ejercicio02_NumerosJustoDespuesDeIntroducirlos.java
+++ src/Capitulo01/Bloque01/Ejercicio02_NumerosJustoDespuesDeIntroducirlos.java
@@ -1,9 +1,23 @@
 package Capitulo01.Bloque01;
 
+import java.util.Scanner;
+
 public class Ejercicio02_NumerosJustoDespuesDeIntroducirlos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduzca un número entero: ");
+		int nEntero = sc.nextInt();
+		System.out.println("El número entero es: " + nEntero);
+		
+		System.out.println("Introduzca un número flotante :");
+		float nFlotante = sc.nextFloat();
+		System.out.println("El número flotante es: " + nFlotante);
+		
+		System.out.println("Introduzca u número doble: ");
+		double nDoble = sc.nextDouble();
+		System.out.println("El número doble es: " + nDoble);
 
 	}
 
